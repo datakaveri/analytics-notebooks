0451d9af059a6f1f70b9d638f0e65f21ec4b437c
Chetan Kumar <96103683+Chetan5276@users.noreply.github.com>
2023-02-03 01:42:58 +0530

    Update pipeline.py

--- Air-Quality/pune-air-quality/src/pune_air_quality/pipelines/deep_learning_interpolation/pipeline.py
+++ Air-Quality/pune-air-quality/src/pune_air_quality/pipelines/deep_learning_interpolation/pipeline.py
@@ -23,5 +23,83 @@ def create_pipeline(**kwargs) -> Pipeline:
                 outputs = None,
                 name = "aqi_deep_learning_model_node",
             ),
+            node(
+                func = deep_learning_model,
+                inputs = ["airTemperature", "known_latlon", "unknown_latlon", "params:deep_learning_model", "params:airTemperature"],
+                outputs = None,
+                name = "at_deep_learning_model_node",
+            ),
+            node(
+                func = deep_learning_model,
+                inputs = ["ambientNoise", "known_latlon", "unknown_latlon", "params:deep_learning_model", "params:ambientNoise"],
+                outputs = None,
+                name = "an_deep_learning_model_node",
+            ),
+            node(
+                func = deep_learning_model,
+                inputs = ["atmosphericPressure", "known_latlon", "unknown_latlon", "params:deep_learning_model", "params:atmosphericPressure"],
+                outputs = None,
+                name = "ap_deep_learning_model_node",
+            ),
+            node(
+                func = deep_learning_model,
+                inputs = ["co", "known_latlon", "unknown_latlon", "params:deep_learning_model", "params:co"],
+                outputs = None,
+                name = "co_deep_learning_model_node",
+            ),
+            node(
+                func = deep_learning_model,
+                inputs = ["co2", "known_latlon", "unknown_latlon", "params:deep_learning_model", "params:co2"],
+                outputs = None,
+                name = "co2_deep_learning_model_node",
+            ),
+            node(
+                func = deep_learning_model,
+                inputs = ["illuminance", "known_latlon", "unknown_latlon", "params:deep_learning_model", "params:illuminance"],
+                outputs = None,
+                name = "il_deep_learning_model_node",
+            ),
+            node(
+                func = deep_learning_model,
+                inputs = ["no2", "known_latlon", "unknown_latlon", "params:deep_learning_model", "params:no2"],
+                outputs = None,
+                name = "no2_deep_learning_model_node",
+            ),
+            node(
+                func = deep_learning_model,
+                inputs = ["o3", "known_latlon", "unknown_latlon", "params:deep_learning_model", "params:o3"],
+                outputs = None,
+                name = "o3_deep_learning_model_node",
+            ),
+            node(
+                func = deep_learning_model,
+                inputs = ["pm2p5", "known_latlon", "unknown_latlon", "params:deep_learning_model", "params:pm2p5"],
+                outputs = None,
+                name = "pm2p5_deep_learning_model_node",
+            ),
+            node(
+                func = deep_learning_model,
+                inputs = ["pm10", "known_latlon", "unknown_latlon", "params:deep_learning_model", "params:pm10"],
+                outputs = None,
+                name = "pm10_deep_learning_model_node",
+            ),
+            node(
+                func = deep_learning_model,
+                inputs = ["so2", "known_latlon", "unknown_latlon", "params:deep_learning_model", "params:so2"],
+                outputs = None,
+                name = "so2_deep_learning_model_node",
+            ),
+            node(
+                func = deep_learning_model,
+                inputs = ["uv", "known_latlon", "unknown_latlon", "params:deep_learning_model", "params:uv"],
+                outputs = None,
+                name = "uv_deep_learning_model_node",
+            ),
+            node(
+                func = deep_learning_model,
+                inputs = ["relativeHumidity", "known_latlon", "unknown_latlon", "params:deep_learning_model", "params:relativeHumidity"],
+                outputs = None,
+                name = "rh_deep_learning_model_node",
+            ),
         ]
     )
